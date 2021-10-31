d216d5d206124c96ba0b54a7701cb23be576d2d7
Evan Carroll <me@evancarroll.com>
2021-10-31 01:10:30 -0500

    minor fixes, more explicit about col names and options

--- repeat--0.0.1.sql
+++ repeat--0.0.1.sql
@@ -3,8 +3,7 @@ RETURNS SETOF int4
 AS 'MODULE_PATHNAME', 'repeat_valuepercall'
 LANGUAGE C STRICT VOLATILE;
 
-CREATE FUNCTION repeat_materialize(IN object int4, IN times int4)
-RETURNS TABLE(x int)
+CREATE FUNCTION repeat_materialize(object int4, times int4)
+RETURNS TABLE(repeat_materialize int4)
 AS 'MODULE_PATHNAME', 'repeat_materialize'
 LANGUAGE C STRICT VOLATILE;
-
