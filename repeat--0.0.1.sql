8d302c205ea43a6ddc4d60df684ed362b6ca01bd
Evan Carroll <me@evancarroll.com>
2021-11-06 17:22:49 -0500

    fix: all implementations have the same signatures

--- repeat--0.0.1.sql
+++ repeat--0.0.1.sql
@@ -1,14 +1,14 @@
-CREATE FUNCTION repeat_valuepercall(n int4, m int4)
-RETURNS TABLE(x int4)
+CREATE FUNCTION repeat_valuepercall(object int4, times int4)
+RETURNS TABLE(repeat int4)
 AS 'MODULE_PATHNAME', 'repeat_valuepercall'
 LANGUAGE C STRICT VOLATILE;
 
 CREATE FUNCTION repeat_materialize(object int4, times int4)
-RETURNS TABLE(repeat_materialize int4)
+RETURNS TABLE(repeat int4)
 AS 'MODULE_PATHNAME', 'repeat_materialize'
 LANGUAGE C STRICT VOLATILE;
 
 CREATE FUNCTION repeat_materialize_preferred(object int4, times int4)
-RETURNS TABLE(x int4)
+RETURNS TABLE(repeat int4)
 AS 'MODULE_PATHNAME', 'repeat_materialize_preferred'
 LANGUAGE C STRICT VOLATILE;
